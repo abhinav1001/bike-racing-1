45895d1f77e2b981989b82d535f68291a4acffb5
abhinav1001 <67742932+abhinav1001@users.noreply.github.com>
2020-09-12 22:26:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var database;
+var gameState=0;
+var playerCount;
+var back,backimg;
+var canvas;
+var distance=0;
+var car ,carImage;
+var track,trackimg;
+var backgroundimg;
+function preload(){
+     carImage=loadImage("images/bike.jpg");
+     trackimg=loadImage("images/track.jpg");
+     backgroundimg=loadImage("images/ground.png")
+}
+function setup(){
+    canvas = createCanvas(displayWidth-20,displayHeight-30);
+
+
+    track=createSprite(550,600,1,1);
+    track.addImage(trackimg);
+    car=createSprite(430,400,100,2);
+    car.addImage(carImage);
+    car.scale=0.2;
+
+   // bike1=new bikes(600,599,100,1);    
+    
+}
+
+function draw(){
+    background(backgroundimg);
+    //bike1.display();
+    camera.y=car.y;
+drawSprites();
+}
+function keyPressed(){
+    /*if(keyDown(LEFT_ARROW)){
+        car.velocityX=-4;
+      
+      }
+       if(keyDown(RIGHT_ARROW)){
+       car.velocityX=4;
+      
+      }*/
+       if(keyDown(UP_ARROW)){
+       car.velocityY=-4;
+       camera.position.x=displayWidth/2;
+       camera.position.y=car.y;
+      }
+      /* if(keyDown(DOWN_ARROW)){
+      car.velocityY=4;
+      
+      }*/
+}
+
